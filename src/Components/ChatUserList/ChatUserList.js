9fe8d21e26bb4e259c275a09262bbfdfe6af81c6
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-04 15:21:26 +0300

    feat: add user images to every UserList item

--- src/Components/ChatUserList/ChatUserList.js
+++ src/Components/ChatUserList/ChatUserList.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import daniealImg from "../../Assets/daniel.jpg";
+import denisImg from "../../Assets/denis.jpg";
+import jimmyImg from "../../Assets/jimmy.jpg";
+import jordanImg from "../../Assets/jordan.jpg";
 
 const ChatUserListContainer = styled.div`
   height: 450px;
@@ -17,24 +21,28 @@ const MessagesTag = styled.h2`
 const usersList = [
   {
     id: 1,
+    userImage: daniealImg,
     name: "daniel",
     message:
       "The band actually formed in 1997, and they already had two albums and a small but loyal fanbase under their belt. That tiny fanbase got a boost almost instantly when their final",
   },
   {
     id: 2,
+    userImage: jordanImg,
     name: "Jordan",
     message:
       "The band actually formed in 1997, and they already had two albums and a small but loyal fanbase under their belt. That tiny fanbase got a boost almost instantly when their final",
   },
   {
     id: 3,
+    userImage: jimmyImg,
     name: "Jimmy",
     message:
       "The band actually formed in 1997, and they already had two albums and a small but loyal fanbase under their belt. That tiny fanbase got a boost almost instantly when their final",
   },
   {
     id: 4,
+    userImage: denisImg,
     name: "Denis",
     message:
       "The band actually formed in 1997, and they already had two albums and a small but loyal fanbase under their belt. That tiny fanbase got a boost almost instantly when their final",
@@ -44,6 +52,7 @@ const usersList = [
 const ListItem = usersList.map((user) => {
   return (
     <li key={user.id.toString()}>
+      <img src={user.userImage} />
       {user.name}
       <p>user.message</p>
     </li>
