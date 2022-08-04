8659d171c0804fa227b9ce0fe4226744e0eb43a5
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-04 15:08:08 +0300

    feat: added a users list to the ChatUserListContainer created an array consisting of users and there details , rendered the users as a list into the container

--- src/Components/ChatUserList/ChatUserList.js
+++ src/Components/ChatUserList/ChatUserList.js
@@ -14,11 +14,48 @@ const MessagesTag = styled.h2`
   font-size: 32px;
 `;
 
+const usersList = [
+  {
+    id: 1,
+    name: "daniel",
+    message:
+      "The band actually formed in 1997, and they already had two albums and a small but loyal fanbase under their belt. That tiny fanbase got a boost almost instantly when their final",
+  },
+  {
+    id: 2,
+    name: "Jordan",
+    message:
+      "The band actually formed in 1997, and they already had two albums and a small but loyal fanbase under their belt. That tiny fanbase got a boost almost instantly when their final",
+  },
+  {
+    id: 3,
+    name: "Jimmy",
+    message:
+      "The band actually formed in 1997, and they already had two albums and a small but loyal fanbase under their belt. That tiny fanbase got a boost almost instantly when their final",
+  },
+  {
+    id: 4,
+    name: "Denis",
+    message:
+      "The band actually formed in 1997, and they already had two albums and a small but loyal fanbase under their belt. That tiny fanbase got a boost almost instantly when their final",
+  },
+];
+
+const ListItem = usersList.map((user) => {
+  return (
+    <li key={user.id.toString()}>
+      {user.name}
+      <p>user.message</p>
+    </li>
+  );
+});
+
 class ChatUserList extends React.Component {
   render() {
     return (
       <ChatUserListContainer data-testid={this.props.testId}>
         <MessagesTag>Messages</MessagesTag>
+        <ul>{ListItem}</ul>
       </ChatUserListContainer>
     );
   }
---
+++ src/Components/ChatUserList/ChatUserList.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import ChatUserList from "./ChatUserList";
+
+describe("Test the components in ChatUserList component succesfully render", () => {
+  test("", () => {});
+});
