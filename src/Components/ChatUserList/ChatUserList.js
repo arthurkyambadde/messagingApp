37d775b1bc5d2a158c481adf468b23bc72d019b0
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-04 13:11:41 +0300

    styles: add styles for chatUserList component

--- src/Components/ChatUserList/ChatUserList.js
+++ src/Components/ChatUserList/ChatUserList.js
@@ -1,8 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const ChatUserListContainer = styled.div`
+  height: 450px;
+  width: 250px;
+  border: 1px solid #fff;
+  border-radius: 8px;
+  padding: 16px;
+`;
 
 class ChatUserList extends React.Component {
   render() {
-    return <div data-testid={this.props.testId}>Its rendering</div>;
+    return (
+      <ChatUserListContainer data-testid={this.props.testId}>
+        Its rendering
+      </ChatUserListContainer>
+    );
   }
 }
 
