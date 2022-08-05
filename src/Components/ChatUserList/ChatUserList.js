1f372696938b6e7ff9e2e462fb497b70f3f657a3
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-05 10:31:20 +0300

    styles: add styles to ChatUserList children components Using styled components added styles to children of the mentioned component and also set the body padding and margin to 0

--- src/Components/ChatUserList/ChatUserList.js
+++ src/Components/ChatUserList/ChatUserList.js
@@ -10,12 +10,60 @@ const ChatUserListContainer = styled.div`
   width: 250px;
   border: 1px solid #fff;
   border-radius: 8px;
-  padding: 16px;
+  /* padding: 16px; */
 `;
 
 const MessagesTag = styled.h2`
   margin: 0;
   font-size: 32px;
+  padding: 16px;
+`;
+
+const UserListItem = styled.li`
+  list-style: none;
+  margin: 0;
+  display: flex;
+  gap: 20px;
+  border-top: 1px solid #777;
+  padding: 10px;
+  &:last-child {
+    border-bottom: 1px solid #777;
+  }
+`;
+
+const UserListContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  /* gap: 20px; */
+  padding: 0px;
+`;
+
+const UserImage = styled.img`
+  min-width: 50px;
+  max-width: 70px;
+  height: 50px;
+  border-radius: 50%;
+  border: 3px solid #fff;
+  background-color: #fff;
+`;
+
+const UserDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  gap: 5px;
+`;
+
+const UserName = styled.span`
+  font-size: 16px;
+  text-transform: capitalize;
+  font-weight: 800;
+  letter-spacing: 1.3px;
+`;
+
+const UserMessages = styled.p`
+  font-size: 10px;
+  margin: 0;
 `;
 
 const usersList = [
@@ -51,11 +99,15 @@ const usersList = [
 
 const ListItem = usersList.map((user) => {
   return (
-    <li key={user.id.toString()}>
-      <img src={user.userImage} />
-      {user.name}
-      <p>user.message</p>
-    </li>
+    <UserListItem key={user.id.toString()}>
+      <UserImage src={user.userImage} />
+      <UserDetails>
+        <UserName>{user.name}</UserName>
+        <UserMessages>
+          {user.message.slice(0, 30).concat("........")}
+        </UserMessages>
+      </UserDetails>
+    </UserListItem>
   );
 });
 
@@ -64,7 +116,7 @@ class ChatUserList extends React.Component {
     return (
       <ChatUserListContainer data-testid={this.props.testId}>
         <MessagesTag>Messages</MessagesTag>
-        <ul>{ListItem}</ul>
+        <UserListContainer>{ListItem}</UserListContainer>
       </ChatUserListContainer>
     );
   }
